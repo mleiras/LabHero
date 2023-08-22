30f51f0cebf5cd9c952876df280b3b8b0b9144d5
mleiras <monicarafaelaleiras@gmail.com>
2023-08-22 17:36:55 +0100

    wip design characters and map

---
+++ data/Bancada.tsx
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="git" name="Bancada" tilewidth="64" tileheight="64" tilecount="9" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="3">
+  <image width="64" height="64" source="../../../../../../Downloads/re (2)/canto superior esquerdo.png"/>
+ </tile>
+ <tile id="4">
+  <image width="64" height="64" source="../../../../../../Downloads/re (2)/canto superior direito.png"/>
+ </tile>
+ <tile id="5">
+  <image width="64" height="64" source="../../../../../../Downloads/re (2)/bancada lateral.png"/>
+ </tile>
+ <tile id="6">
+  <image width="64" height="64" source="../../../../../../Downloads/re (2)/bancada lateral fim.png"/>
+ </tile>
+ <tile id="7">
+  <image width="64" height="64" source="../../../../../../Downloads/re (2)/bancada horizontal.png"/>
+ </tile>
+ <tile id="8">
+  <image width="64" height="64" source="../../../../../../Downloads/re (2)/bancada horizontal esquerda.png"/>
+ </tile>
+ <tile id="9">
+  <image width="64" height="64" source="../../../../../../Downloads/re (2)/bancada horizontal direita.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.909091" y="0.727273" width="62" height="62.3636"/>
+  </objectgroup>
+ </tile>
+ <tile id="10">
+  <image width="64" height="64" source="../../../../../../Downloads/pc up.png"/>
+ </tile>
+ <tile id="11">
+  <image width="64" height="64" source="../../../../../../Downloads/pc up (1).png"/>
+ </tile>
+</tileset>
---
+++ data/Cientista 1.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="git" name="Cientista 1" tilewidth="64" tileheight="64" tilecount="4" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="64" height="64" source="../../../../../../Downloads/re/cientista 1.png"/>
+ </tile>
+ <tile id="1">
+  <image width="64" height="64" source="../../../../../../Downloads/re/cientista 2.png"/>
+ </tile>
+ <tile id="2">
+  <image width="64" height="64" source="../../../../../../Downloads/re/cientista 3.png"/>
+ </tile>
+ <tile id="5">
+  <image width="64" height="64" source="../../../../../../Downloads/re/cientista 2.png"/>
+ </tile>
+</tileset>
