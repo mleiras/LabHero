76a7afc3ff985e6b627e14c12c904c7189dedc47
mleiras <monicarafaelaleiras@gmail.com>
2023-10-14 11:38:20 +0100

    cleaning files rep

---
+++ data/Tilesets/Objetos Novos.tsx
@@ -0,0 +1,130 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="git" name="Objetos Novos" tilewidth="64" tileheight="65" tilecount="42" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="1">
+  <image width="64" height="64" source="../../graphics/environment/canto superior esquerdo.png"/>
+ </tile>
+ <tile id="2">
+  <image width="64" height="64" source="../../graphics/environment/canto superior direito.png"/>
+ </tile>
+ <tile id="3">
+  <image width="64" height="64" source="../../graphics/environment/bancada lateral.png"/>
+ </tile>
+ <tile id="4">
+  <image width="64" height="64" source="../../graphics/environment/bancada lateral fim.png"/>
+ </tile>
+ <tile id="5">
+  <image width="64" height="64" source="../../graphics/environment/bancada horizontal.png"/>
+ </tile>
+ <tile id="6">
+  <image width="64" height="64" source="../../graphics/environment/bancada horizontal esquerda.png"/>
+ </tile>
+ <tile id="7">
+  <image width="64" height="64" source="../../graphics/environment/bancada horizontal direita.png"/>
+ </tile>
+ <tile id="8">
+  <image width="64" height="64" source="../../graphics/environment/computer.png"/>
+ </tile>
+ <tile id="9">
+  <image width="64" height="64" source="../../graphics/environment/sofá esquerda.png"/>
+ </tile>
+ <tile id="10">
+  <image width="64" height="64" source="../../graphics/environment/sofá direita.png"/>
+ </tile>
+ <tile id="12">
+  <image width="64" height="64" source="../../graphics/environment/microscopio.png"/>
+ </tile>
+ <tile id="13">
+  <image width="64" height="64" source="../../graphics/environment/maquina de café.png"/>
+ </tile>
+ <tile id="14">
+  <image width="64" height="64" source="../../graphics/environment/estante livros up.png"/>
+ </tile>
+ <tile id="15">
+  <image width="64" height="64" source="../../graphics/environment/estante livros down.png"/>
+ </tile>
+ <tile id="16">
+  <image width="64" height="64" source="../../graphics/environment/Pc-left.png"/>
+ </tile>
+ <tile id="17">
+  <image width="64" height="64" source="../../graphics/environment/Pc-right.png"/>
+ </tile>
+ <tile id="18">
+  <image width="64" height="64" source="../../graphics/environment/pc down.png"/>
+ </tile>
+ <tile id="19">
+  <image width="64" height="64" source="../../graphics/environment/black_chair_left.png"/>
+ </tile>
+ <tile id="20">
+  <image width="64" height="64" source="../../graphics/environment/black_chair_right.png"/>
+ </tile>
+ <tile id="25">
+  <image width="64" height="64" source="../../graphics/environment/Pc-left_2.png"/>
+ </tile>
+ <tile id="26">
+  <image width="64" height="64" source="../../graphics/environment/Pc-right_2.png"/>
+ </tile>
+ <tile id="33">
+  <image width="64" height="64" source="../../graphics/environment/pc down_2.png"/>
+ </tile>
+ <tile id="34">
+  <image width="64" height="64" source="../../graphics/environment/pc down_3.png"/>
+ </tile>
+ <tile id="35">
+  <image width="64" height="64" source="../../graphics/environment/pc down_4.png"/>
+ </tile>
+ <tile id="36">
+  <image width="64" height="64" source="../../graphics/environment/pc down_5.png"/>
+ </tile>
+ <tile id="37">
+  <image width="64" height="64" source="../../graphics/environment/computer.png"/>
+ </tile>
+ <tile id="38">
+  <image width="64" height="64" source="../../graphics/environment/maquina de café.png"/>
+ </tile>
+ <tile id="39">
+  <image width="64" height="64" source="../../graphics/environment/sofá esquerda.png"/>
+ </tile>
+ <tile id="40">
+  <image width="64" height="64" source="../../graphics/environment/sofá direita.png"/>
+ </tile>
+ <tile id="47">
+  <image width="64" height="64" source="../../graphics/environment/tabela_periodica_left.png"/>
+ </tile>
+ <tile id="48">
+  <image width="64" height="64" source="../../graphics/environment/tabela_periodica_right.png"/>
+ </tile>
+ <tile id="49">
+  <image width="64" height="64" source="../../graphics/environment/quadro_dna.png"/>
+ </tile>
+ <tile id="51">
+  <image width="64" height="64" source="../../graphics/environment/flasks.png"/>
+ </tile>
+ <tile id="52">
+  <image width="64" height="64" source="../../graphics/environment/flasks_2.png"/>
+ </tile>
+ <tile id="53">
+  <image width="64" height="64" source="../../graphics/environment/flasks_3.png"/>
+ </tile>
+ <tile id="54">
+  <image width="64" height="65" source="../../graphics/environment/reator_down.png"/>
+ </tile>
+ <tile id="55">
+  <image width="64" height="65" source="../../graphics/environment/reator_top.png"/>
+ </tile>
+ <tile id="56">
+  <image width="64" height="64" source="../../graphics/environment/pc down_6.png"/>
+ </tile>
+ <tile id="57">
+  <image width="64" height="64" source="../../graphics/environment/black_chair_back.png"/>
+ </tile>
+ <tile id="58">
+  <image width="64" height="64" source="../../graphics/environment/flasks_4.png"/>
+ </tile>
+ <tile id="59">
+  <image width="64" height="64" source="../../graphics/environment/pc down_7.png"/>
+ </tile>
+ <tile id="60">
+  <image width="64" height="64" source="../../graphics/environment/microscopio_2.png"/>
+ </tile>
+</tileset>
--- data/testye.tsx
+++
@@ -1,2 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="git" name="testye" tilewidth="1" tileheight="1" tilecount="0" columns="0"/>
